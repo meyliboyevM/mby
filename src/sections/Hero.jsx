1b7e706b9f88dda732fd11cc1175df069403dd76
meyliboyev <meyliboyy@gamil.com>
2025-06-30 16:43:32 +0500

    changed import file name

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import HeroText from "../components/HeroText";
-import ParallaxBackground from "../components/ParallaxBackground";
+import ParallaxBackground from "../components/ParallaxBackground.jsx";
 
 const Hero = () => {
   return (
