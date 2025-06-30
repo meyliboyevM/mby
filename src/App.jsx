c7b4b49ee3f97841d4809248d8049994d2543587
meyliboyev <meyliboyy@gamil.com>
2025-06-30 16:37:58 +0500

    project cleared

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "react";
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
-import About from "./sections/About";
-import Projects from "./sections/Projects";
-import Experiences from "./sections/Experiences";
-import Testimonial from "./sections/Testimonial";
-import Contact from "./sections/Contact";
 import Footer from './sections/Footer';
 
 const App = () => {
@@ -13,11 +8,6 @@ const App = () => {
     <div className="container mx-auto max-w-7xl">
       <Navbar />
       <Hero />
-      {/*<About />*/}
-      {/*<Projects />*/}
-      {/*<Experiences />*/}
-      {/*<Testimonial />*/}
-      {/*<Contact />*/}
       <Footer/>
     </div>
   );
